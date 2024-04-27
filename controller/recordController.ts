49ef4055e92713e68ef344698ed1c0ae03fb98b2
Arpan Das <dasarpan471@gmail.com>
2024-04-27 10:35:34 +0530

    skeleton function is created for record function

---
+++ controller/recordController.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { errorResponse } from "../utils/response";
+
+export const createRecord = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch (e) {
+        console.error("Authorization Error:", e);
+        errorResponse(500, "Internal Server Error", e as Error, res);
+    }
+}
+
+export const getRecord = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch (e) {
+        console.error("Authorization Error", e)
+        errorResponse(500, "Internal Server Error", e as Error, res);
+    }
+}
+
+export const updateRecord = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch (e) {
+        console.error("Authorization Error", e)
+        errorResponse(500, "Internal Server Error", e as Error, res);
+    }
+}
